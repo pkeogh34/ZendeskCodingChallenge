3ee71fbec2cda9316edfe6b4aa685a8f3128c277
pkeogh34 <patrick.keogh1@ucdconnect.ie>
2021-11-23 14:21:02 +0000

    Added Ticket class to create objects to hold data on individual tickets

---
+++ Ticket.py
@@ -0,0 +1,20 @@
+class Ticket:
+    def __init__(self, ticket_id, created, updated, ticket_type,  subject, description, status, tags,
+                 requester_id):
+        self._ticket_id = ticket_id
+        self.created = created
+        self.updated = updated
+        self.ticket_type = ticket_type
+        self.subject = subject
+        self.description = description
+        self.status = status
+        self.tags = tags
+        self.requester_id = requester_id
+
+    def to_string(self):
+        return ("Ticket ID: %s\n"
+                "Subject: %s" % (self._ticket_id, self.subject))
+
+    @property
+    def ticket_id(self):
+        return self._ticket_id
--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-import json, requests
+from tkinter import WORD, INSERT
 from urllib.parse import urlencode
+import tkinter as tk
+
+from Data import Data
 
 subdomain = "zcczendeskcodingchallenge6845"
 encoded = "cGF0cmljay5rZW9naDFAdWNkY29ubmVjdC5pZS90b2tlbjp1a05KSlVBaFRiMzRoMmZZejVuVTNGcVdEN2NTSWh1MEd5dENLQm1T"
 url_prefix = "https://%s.zendesk.com/api/v2/search.json" % subdomain
 url_headers = {'Content-Type': 'application/json', 'Authorization': 'Basic %s' % encoded}
 parameters = {'query': 'type:ticket', 'sort_by': 'created_at', 'sort_order': 'asc'}
-
-
-# function to get json data from the Zendesk API
-def fetch(url, params={}):
-    # construct the url
-    url += "?" + urlencode(params)
-    print("Getting tickets from %s" % url)
-    # fetch the page
-    response = requests.get(url, headers=url_headers)
-    jdata = response.text
-    return json.loads(jdata)
-
-
-fetch(url_prefix, parameters)
+ticket_data = {}
+w_width = 500
+w_height = 350
 
