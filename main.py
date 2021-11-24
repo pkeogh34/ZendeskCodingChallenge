79bfb489e0594f5ad15d99bf5e1817e018c307fd
pkeogh34 <patrick.keogh1@ucdconnect.ie>
2021-11-24 17:23:24 +0000

    Defined create_tickets function to add tickets to window and implemented pagination in the window

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from tkinter import WORD, INSERT, RIGHT, Y, LEFT
 from urllib.parse import urlencode
 import tkinter as tk
 
+from requests.exceptions import InvalidSchema
+
 from Data import Data
 
 subdomain = "zcczendeskcodingchallenge6845"
-encoded = "cGF0cmljay5rZW9naDFAdWNkY29ubmVjdC5pZS90b2tlbjp1a05KSlVBaFRiMzRoMmZZejVuVTNGcVdEN2NTSWh1MEd5dENLQm1T"
+encoded = "GF0cmljay5rZW9naDFAdWNkY29ubmVjdC5pZS90b2tlbjp1a05KSlVBaFRiMzRoMmZZejVuVTNGcVdEN2NTSWh1MEd5dENLQm1T"
 url_prefix = "https://%s.zendesk.com/api/v2/search.json" % subdomain
 url_headers = {'Content-Type': 'application/json', 'Authorization': 'Basic %s' % encoded}
 parameters = {'query': 'type:ticket', 'sort_by': 'created_at', 'sort_order': 'asc'}
@@ -19,20 +21,8 @@ def get_url(url, params={}):
     return url
 
 
-def centre_window(window, width, height):
-    display_width = window.winfo_screenwidth()
-    display_height = window.winfo_screenheight()
-    x = (display_width / 2) - (width / 2)
-    y = (display_height / 2) - (height / 2)
-    window.geometry("{}x{}+{}+{}".format(width, height, int(x), int(y)))
-
-
-def blue_text(event=None):
-    lab.config(fg="blue")
-
-
-def black_text(event=None):
-    lab.config(fg="black")
+def close_window(event=None):
+    root.destroy()
 
 
 def get_data(prefix, headers, params={}) -> object:
@@ -44,7 +34,24 @@ def get_data(prefix, headers, params={}) -> object:
 def get_data_helper(event=None):
     global ticket_data
     ticket_data = get_data(url_prefix, url_headers, parameters)
+
     ticket_frame.destroy()
+
+
+def blue_text(event=None):
+    lab.config(fg="purple")
+
+
+def black_text(event=None):
+    lab.config(fg="blue")
+
+
+def centre_window(window, width, height):
+    display_width = window.winfo_screenwidth()
+    display_height = window.winfo_screenheight()
+    x = (display_width / 2) - (width / 2)
+    y = (display_height / 2) - (height / 2)
+    window.geometry("{}x{}+{}+{}".format(width, height, int(x), int(y)))
 
 
 def open_ticket(num, event=None):
@@ -60,6 +67,64 @@ def open_ticket(num, event=None):
     text.config(state='disabled')
     text.grid()
     ticket_root.mainloop()
+
+
+def create_buttons():
+    canvas = tk.Canvas(ticket_frame)
+    scrollbar = tk.Scrollbar(ticket_frame, orient="vertical", command=canvas.yview)
+    scrollable_frame = tk.Frame(canvas)
+
+    scrollable_frame.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+
+    canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
+    canvas.configure(yscrollcommand=scrollbar.set)
+
+    for i in range(25):
+        if (i + j) < len(ticket_data):
+            ticket_btn = tk.Button(master=scrollable_frame, text=ticket_data[i + j].subject, width=100)
+
+            def handler(event, num=i + j):
+                return open_ticket(num, event)
+
+            ticket_btn.bind('<Button-1>', handler)
+            ticket_btn.pack()
+
+    canvas.pack(side="left", fill="both", expand=True)
+    scrollbar.pack(side="right", fill="y")
+
+    if j >= 0 and ((j + 25) < len(ticket_data)):
+        btn = tk.Button(master=pagination_frame, text="next")
+        btn.bind('<Button-1>', next_page)
+        btn.pack(side="right")
+    if j > 0:
+        btn = tk.Button(master=pagination_frame, text="previous")
+        btn.bind('<Button-1>', previous_page)
+        btn.pack(side="left")
+
+    ticket_frame.pack(fill="both", expand=True)
+    pagination_frame.pack()
+
+
+def next_page(event=None):
+    global j, ticket_frame, pagination_frame
+    j += 25
+    refresh()
+    create_buttons()
+
+
+def previous_page(event=None):
+    global j, ticket_frame, pagination_frame
+    j -= 25
+    refresh()
+    create_buttons()
+
+
+def refresh():
+    global ticket_frame, pagination_frame
+    ticket_frame.destroy()
+    pagination_frame.destroy()
+    ticket_frame = tk.Frame(master=root)
+    pagination_frame = tk.Frame(master=root)
 
 
 root = tk.Tk()
@@ -90,4 +155,8 @@ lab.pack()
 ticket_frame.pack()
 root.wait_window(lab)
 ticket_frame = tk.Frame(master=root)
+pagination_frame = tk.Frame(master=root)
+
+j = 0
+create_buttons()
 root.mainloop()
