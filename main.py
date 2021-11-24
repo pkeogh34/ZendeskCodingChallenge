eba3b6b34be34c37f3d2c19cbf538a19fff9ca51
pkeogh34 <patrick.keogh1@ucdconnect.ie>
2021-11-24 14:17:11 +0000

    Added main label for window and function centre on screen

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import WORD, INSERT
+from tkinter import WORD, INSERT, RIGHT, Y, LEFT
 from urllib.parse import urlencode
 import tkinter as tk
 
@@ -10,6 +10,34 @@ url_prefix = "https://%s.zendesk.com/api/v2/search.json" % subdomain
 url_headers = {'Content-Type': 'application/json', 'Authorization': 'Basic %s' % encoded}
 parameters = {'query': 'type:ticket', 'sort_by': 'created_at', 'sort_order': 'asc'}
 ticket_data = {}
-w_width = 500
-w_height = 350
+w_width = 700
+w_height = 450
 
+
+def get_url(url, params={}):
+    url += "?" + urlencode(params)
+    return url
+
+
+def centre_window(window, width, height):
+    display_width = window.winfo_screenwidth()
+    display_height = window.winfo_screenheight()
+    x = (display_width / 2) - (width / 2)
+    y = (display_height / 2) - (height / 2)
+    window.geometry("{}x{}+{}+{}".format(width, height, int(x), int(y)))
+
+
+root = tk.Tk()
+root.geometry("%dx%d" % (w_width, w_height))
+root.resizable(False, False)
+centre_window(root, w_width, w_height)
+title_frame = tk.Frame(master=root)
+title_lbl = tk.Label(
+    master=title_frame,
+    text="Ticket Viewer",
+    font=('Helvetica bold', 35)
+)
+title_lbl.grid()
+title_frame.pack()
+
+root.mainloop()
